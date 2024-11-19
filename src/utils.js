f8d847587d4d389752382e803dd0093a983e7df7
Jordan Bütschi <jordan.butschi@4soft.ch>
2024-11-19 11:48:29 +0100

    [Frontend] Utils fonctions
    Ajoute getUrlImage
    Ajoute setUrlParams

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,15 @@ export const BASE_URL_API = `${BASE_URL}/api/`
 export const spinner = (color = "#fff") => {
     return <FontAwesomeIcon spin icon={faSync} style={{ color }} />
 }
+
+export const getUrlImage = (src) => {
+    return BASE_URL + src
+}
+
+export const setUrlParams = (_url, params) => {
+    let url = new URL(_url)
+    for (const [key, value] of Object.entries(params)) {
+        url.searchParams.append(key, value)
+    }
+    return url
+}
